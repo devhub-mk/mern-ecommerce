392803059d22a134c3278f5f42fdc5be6ffdb3b2
Mahender Kumar <mahender.codedrill@gmail.com>
2023-10-30 13:01:39 +0530

    Cart Page added

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -12,6 +12,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage"
 import SignupPage from "./pages/SignupPage"
+import CartPage from "./pages/CartPage";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage />,
+  },
+  {
+    path: "/cart",
+    element: <CartPage />,
   },
 ]);
 
