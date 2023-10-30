6773ea5d830bb1c9483b36fd726710e242a2788b
Mahender Kumar <mahender.codedrill@gmail.com>
2023-10-30 12:12:57 +0530

    Initial commit with login and signup page

---
+++ react-ecommerce/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage"
+import SignupPage from "./pages/SignupPage"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-ecommerce/src/pages/SignupPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Signup from "../features/auth/components/Signup";
+
+const SignupPage = () => {
+  return <div><Signup/></div>;
+};
+
+export default SignupPage;
